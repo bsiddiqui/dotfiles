f39ad8f3e42aabcaa13ef877204bcf53cede0cb6
Basil Siddiqui <basil.siddiqui@gmail.com>
2014-02-06 11:40:08 -0800

    added symlink for aliases functions and gitconfig

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".tmux.conf .zshrc .vimrc .vim"    # list of files/folders to symlink in homedir
+files=".tmux.conf .zshrc .vimrc .vim .aliases .functions .gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
