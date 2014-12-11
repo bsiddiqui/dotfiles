d414f721fb8c715d94250130bb95fd348cbcce0f
Basil Siddiqui <basil.siddiqui@gmail.com>
2014-12-11 00:11:44 -0800

    refactor(vim): removed ycm and vim scripts

--- devsetup.sh
+++ devsetup.sh
@@ -27,9 +27,9 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    mv ~/$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+  mv ~/$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/$file
 done
 echo "done"
 
@@ -48,37 +48,39 @@ echo "installing vim bundles"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 # compile YCM
-cd ~/dotfiles/.vim/bundle/YouCompleteMe && ./install.sh
+# cd ~/dotfiles/.vim/bundle/YouCompleteMe
+# git submodule update --init --recursive
+# ./install.sh
 # setup tern server
 cd ~/.vim/bundle/tern_for_vim && npm install
 echo "done"
 
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+  # Test to see if zshell is installed.  If it is:
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone the oh-my-zsh repository from GitHub only if it isn't already present
     if [[ ! -d ~/.oh-my-zsh/ ]]; then
-        echo "installing oh-my-zsh"
-        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-        echo "done"
+      echo "installing oh-my-zsh"
+      git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+      echo "done"
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
+      chsh -s $(which zsh)
     fi
-else
+  else
     # If zsh isn't installed, get the platform of the current machine
     platform=$(uname);
     # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
+      sudo apt-get install zsh
+      install_zsh
+      # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
+      echo "Please install zsh, then re-run this script!"
+      exit
     fi
-fi
+  fi
 }
 
 install_zsh
